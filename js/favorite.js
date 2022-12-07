d8eaa25c9d9b1c3bef7191171804cb65a39e8107
Luis Rodrigues <103431660+devluisrodrigues@users.noreply.github.com>
2022-12-06 22:51:32 -0300

    curtidas funcionando

--- js/favorite.js
+++ js/favorite.js
@@ -9,20 +9,29 @@ document.addEventListener('DOMContentLoaded', function() {
     main = body.querySelector('main');
     ul = main.querySelector('ul.linhaaround');
     btn = ul.querySelector('li.addtofav');
+    img_fav = btn.querySelector('img');
+    console.log(img_fav);
     
-    console.log(localStorage.getItem(filme));
+    
     if(localStorage.getItem(filme) == null) {
         console.log('Filme não favoritado');
         localStorage.setItem(filme, false);
     }
 
+    console.log(localStorage.getItem(filme));
+    if(localStorage.getItem(filme)) {
+        img_fav.src = '../img/listaativo.png';
+    }
+
     btn.addEventListener('click', function(event) {
         event.stopPropagation();
         if(localStorage.getItem(filme) == 'false') {
-        console.log(filme);
-        localStorage.setItem(filme, true);
+            console.log(filme);
+            localStorage.setItem(filme, true);
+            img_fav.src = '../img/listaativo.png';
         } else {
             localStorage.setItem(filme, false);
+            img_fav.src = '../img/addtolist.png';
         }
         
     });

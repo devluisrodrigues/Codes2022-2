0770119c353bde2c2cc3f8568dc66d07e06cbb43
Luis Rodrigues <103431660+devluisrodrigues@users.noreply.github.com>
2022-12-06 23:26:04 -0300

    calendario uhu

--- js/favorite_insert.js
+++ js/favorite_insert.js
@@ -2,10 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     body = document.querySelector('body');
     ul = body.querySelector('ul.coluna');
     li = ul.querySelector('li.linha');
+    
 
     while (ul.firstChild) {
         ul.removeChild(ul.firstChild);
     }
+
 
     catalogo = localStorage.getItem('filmes');
     catalogo = JSON.parse(catalogo);
@@ -35,4 +37,49 @@ document.addEventListener('DOMContentLoaded', function() {
             ul.appendChild(clone);
         }
     }
+
+
+
+
+
+
+    ul_calend = body.querySelector('ul.carrosel');
+    li_calend = ul_calend.querySelector('li');
+    console.log(li_calend)
+    clone_calend = li_calend.cloneNode(true);
+
+
+    console.log(ul_calend)
+    while (ul_calend.firstChild) {
+        ul_calend.removeChild(ul_calend.firstChild);
+    }
+
+        for(titulo in keys) {
+            if(keys[titulo] != 'filmes' && localStorage.getItem(keys[titulo]) == 'true') {
+                data = catalogo[keys[titulo]]['data'];
+                if(data == undefined){
+                    console.log('Filme já foi lançado');
+                } else {
+                    console.log(data);
+                    clone_calend = li_calend.cloneNode(true);
+                    texto_calend = clone_calend.querySelector('p');
+                    texto_calend.innerHTML = keys[titulo];
+
+                    texto_data = document.createElement('p');
+                    texto_data.classList.add('tex');
+
+                    texto_data.innerHTML = data;
+                    texto_calend.appendChild(texto_data);
+
+                    ul_calend.appendChild(clone_calend);
+                }
+            }
+    }
+
+    if (ul_calend.firstChild == null) {
+        texto_calend = clone_calend.querySelector('p');
+        texto_calend.innerHTML = 'Nenhum título ainda não lançado foi favoritado';
+        ul_calend.appendChild(clone_calend);
+    }
+
 });
